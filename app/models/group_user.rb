935b5351ba6216a35a25db4fa69a9f643d7906a3
五島成美 <narumi.goto@di-v.co.jp>
2019-06-15 16:23:43 +0900

    非同期通信

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,15 +1,4 @@
-class CreateGroupUsers < ActiveRecord::Migration[5.0]
-    class GroupUser < ApplicationRecord
-        belongs_to :group
-        belongs_to :user
-      end
-      
-    def change
-      create_table :group_users do |t|
-        t.references :group, foreign_key: true
-        t.references :user, foreign_key: true
-        t.timestamps
-      end
-    end
-  end
-  
+class GroupUser < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+end
